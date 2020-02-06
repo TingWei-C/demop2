a90764689e77938c6d93707f9738881cadc36ea0
TingWei-C <55474142+TingWei-C@users.noreply.github.com>
2020-02-06 22:01:23 +0800

    Initial

---
+++ FriendMVC/Controllers/HomeNewController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FriendMVC.Controllers
+{
+    public class HomeNewController : Controller
+    {
+        public ActionResult HomeIndex()
+        {
+            return View("ViewPage1");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
